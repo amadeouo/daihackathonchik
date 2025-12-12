2acad7d7643b6e9dc7b293b0580432221840a25b
Mikhail Volodko <clavor.32@gmail.com>
2025-12-13 02:40:32 +0300

    add instance and interceptors for axios

---
+++ frontend/src/shared/utils/api.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3001',
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      try {
+        await api.post('/auth/refresh')
+        return api(originalRequest)
+      } catch (refreshError: any) {
+        if (refreshError.response?.status === 401 || refreshError.response?.status === 422) {
+          throw Error('Не получилось получить данные пользователя')
+        }
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
