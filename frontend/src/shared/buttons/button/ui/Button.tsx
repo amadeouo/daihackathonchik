9ac07ea659b1bdc60f8b949aab67bf9290deec52
Mikhail Volodko <clavor.32@gmail.com>
2025-12-28 03:53:07 +0300

    refactor: add onSubmit props

--- frontend/src/shared/buttons/button/ui/Button.tsx
+++ frontend/src/shared/buttons/button/ui/Button.tsx
@@ -7,6 +7,7 @@ type buttonPropsTypes = {
   className?: string | object,
   submit?: boolean,
   onClick?: () => void
+  onSubmit?: () => void
 }
 
 export const Button = (props: buttonPropsTypes) => {
@@ -15,6 +16,7 @@ export const Button = (props: buttonPropsTypes) => {
     className,
     submit,
     onClick,
+    onSubmit,
   } = props
 
   return (
@@ -22,6 +24,7 @@ export const Button = (props: buttonPropsTypes) => {
       className={classNames(className, classes.button)}
       type={submit ? "submit" : "button"}
       onClick={onClick}
+      onSubmit={onSubmit}
     >
       {children}
     </button>
